0f955f186b3058ae86b036defe3bd651f333f7da
Jonatas <jonataslimafsa@gmail.com>
2022-07-03 14:49:17 -0300

    update router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,7 @@
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
